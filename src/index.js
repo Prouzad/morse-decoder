0e7c9c24aead8ce9971c48e7920c62abf7e04f38
Abdulla <94592483+Prouzad@users.noreply.github.com>
2022-02-22 08:28:26 +0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -38,7 +38,54 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+        let arr = [];
+    let arr2 = [];
+    let gr = [];
+    let morz = [];
+    for(let i = 0; i < expr.length; i += 10){
+        let someUsers = expr.slice(i, i+10)
+        
+        arr.push(someUsers);
+        
+        
+    }
+
+    let sdf = arr.join('; ')
+    arr2.push(sdf)
+    
+    for(let g = 0; g < arr2.length; g++){
+       
+        for(let a = 0; a < arr2[g].length; a +=2 )
+       
+        gr.push(arr2[g][a]+arr2[g][a+1])
+        
+    }
+    for(let number of gr )
+    if(number.includes(10)){
+        morz.push('.')
+    }else if(number.includes(11)){
+        morz.push('-')
+    }else if(number.includes('*')){
+        morz.push('')
+    }else if(number.includes('; ')){
+        morz.push('; ')
+    }
+    let m = morz.join()
+    let du = m.replace(/(,*)(;*)/g, '');
+    let v = du.split(" ")
+    
+    for(let d = 0; d < v.length; d++){
+       
+           let ind = key.indexOf(v[d]);
+       v[d] = value[ind];
+       
+    }
+    for(let k = 0; k < v.length; k++){
+      if(v[k] == undefined){
+          v[k] = ' '
+      }
+    }
+    return v.toString().replace(/(,*)/g, '')
 }
 
 module.exports = {
